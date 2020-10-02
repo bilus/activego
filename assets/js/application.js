f8f177d1f5559695987c592c1806dde45f3da909
Marcin Bilski <gyamtso@gmail.com>
2020-10-02 14:42:56 +0200

    Install ActionCable client for experimentation.

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,7 +1,10 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 require("@fortawesome/fontawesome-free/js/all.js");
+ActionCable = require("actioncable");
 
 $(() => {
+    this.App || (this.App = {});
 
+    this.App.cable = ActionCable.createConsumer("ws://localhost:8080");
 });
